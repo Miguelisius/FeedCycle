db0c7cb0a4066131f190b8f4921897f887268c8b
Miguelisius <miguelgonzalezlopezz@gmail.com>
2024-10-04 10:56:46 +0200

    Cambios en admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Project, Task, Tutor, Alumno
+from .models import Project, Task, Tutor, Alumno, Grupo, Rubrica, Criterios, NivelDeDesempeno, Descriptores, Notas, Calificacion
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Tutor)
 admin.site.register(Alumno)
+admin.site.register(Grupo)
+admin.site.register(Rubrica)
+admin.site.register(Criterios)
+admin.site.register(NivelDeDesempeno)
+admin.site.register(Descriptores)
+admin.site.register(Notas)
+admin.site.register(Calificacion)
