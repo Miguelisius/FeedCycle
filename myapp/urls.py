604a1e1c9c06cbed46dbcba1042f7f0de2fa32d7
Miguelisius <miguelgonzalezlopezz@gmail.com>
2024-12-07 17:27:48 +0100

    funciona logout

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('inicio/', views.home, name='home'),
     path('project/', views.projects, name='projects'),
     path('project_detail/<int:project_id>/group/<int:group_id>/', views.project_detail, name='project_detail'),
--- tfg/settings.py
+++ tfg/settings.py
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 LOGIN_REDIRECT_URL = '/inicio/'
 LOGOUT_REDIRECT_URL = 'login'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
